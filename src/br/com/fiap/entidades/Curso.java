f80703893fd943aae8399fd39353f93f82621aba
logonrm <logonrm@fiap.com.br>
2018-06-25 21:30:10 -0300

    Alterando GenerationType para identity e configuração do Hibernate para não tentar criar o database

--- src/br/com/fiap/entidades/Curso.java
+++ src/br/com/fiap/entidades/Curso.java
@@ -16,7 +16,7 @@ import javax.persistence.TemporalType;
 @Table(name="curso")
 public class Curso {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
 	
--- src/br/com/fiap/entidades/Escola.java
+++ src/br/com/fiap/entidades/Escola.java
@@ -24,7 +24,7 @@ import javax.persistence.TemporalType;
 @Table(name="escola")
 public class Escola {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
 	
